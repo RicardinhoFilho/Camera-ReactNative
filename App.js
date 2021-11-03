489dbfc3390609de1df19d225f804776ad5c5759
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-11-03 11:37:25 -0300

    First COmmit -am

--- App.js
+++ App.js
@@ -1,21 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Modal,
+  Image
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
+  const camRef = useRef(null);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Esperando</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>Acesso Negado!</Text>;
+  }
+
+  async function takePickture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      console.log(data);
+      setPhoto(data.uri);
+      setOpen(true);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+        <View
+          styles={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            style={{ position: "absolute", bottom: 20, left: 20 }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 20, marginBottom: 13, color: "#FFF" }}>
+              Trocar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <View
+        styles={{
+          flex: 1,
+          backgroundColor: "transparent",
+          flexDirection: "row",
+        }}
+      >
+        <TouchableOpacity
+          style={{ position: "absolute", bottom: 20, left: 20 }}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        >
+          <Text style={{ fontSize: 20, marginBottom: 13, color: "#FFF" }}>
+            Trocar
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#121212",
+          margin: 20,
+          borderRadius: 10,
+          height: 50,
+        }}
+        onPress={takePickture}
+      >
+        <FontAwesome name="camera" size={23} color="#fff" />
+      </TouchableOpacity>
+
+      {photo && (
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 20,
+            }}
+          >
+            <TouchableOpacity
+              style={{ margin: 10 }}
+              onPress={() => setOpen(false)}
+            >
+              <FontAwesome name="window-close" size={50} color="#FF0000" />
+            </TouchableOpacity>
+            <Image
+              style={{ width: "100%", height: 300, borderRadius: 20 }}
+              source={{ uri: photo }}
+            />
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
